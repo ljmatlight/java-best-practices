24af19077d2e125b4da4622e56140b513d103cc3
ljmatlight <vimx86@gmail.com>
2018-03-09 10:30:24 +0800

    Lambda and Collections junit

---
+++ java-features-examples-guides/src/test/java/com/github/ljmatlight/features/jdk8/LambdaAndCollectionsDemoTest.java
@@ -0,0 +1,105 @@
+package com.github.ljmatlight.features.jdk8;
+
+import org.junit.Test;
+
+/**
+ * Description：
+ * <br /> Author： galsang
+ */
+public class LambdaAndCollectionsDemoTest extends LambdaAndCollectionsDemo {
+    @Test
+    public void testForJDK7() throws Exception {
+        super.testForJDK7();
+    }
+
+    @Test
+    public void testForEach() throws Exception {
+        super.testForEach();
+    }
+
+    @Test
+    public void testForEachLambda() throws Exception {
+        super.testForEachLambda();
+    }
+
+    @Test
+    public void testRemoveIfJDK7() throws Exception {
+        super.testRemoveIfJDK7();
+    }
+
+    @Test
+    public void testRemoveIf() throws Exception {
+        super.testRemoveIf();
+    }
+
+    @Test
+    public void testRemoveIfLambda() throws Exception {
+        super.testRemoveIfLambda();
+    }
+
+    @Test
+    public void testReplaceAllJDK7() throws Exception {
+        super.testReplaceAllJDK7();
+    }
+
+    @Test
+    public void testReplaceAll() throws Exception {
+        super.testReplaceAll();
+    }
+
+    @Test
+    public void testReplaceAllLambda() throws Exception {
+        super.testReplaceAllLambda();
+    }
+
+    @Test
+    public void testSortJDK7() throws Exception {
+        super.testSortJDK7();
+    }
+
+    @Test
+    public void testSortLambda() throws Exception {
+        super.testSortLambda();
+    }
+
+    @Test
+    public void testMapForEachJDK7() throws Exception {
+        super.testMapForEachJDK7();
+    }
+
+    @Test
+    public void testMapForEach() throws Exception {
+        super.testMapForEach();
+    }
+
+    @Test
+    public void testMapForEachLambda() throws Exception {
+        super.testMapForEachLambda();
+    }
+
+    @Test
+    public void testMapGetOrDefault() throws Exception {
+        super.testMapGetOrDefault();
+    }
+
+    @Test
+    public void testMapReplaceAllJDK7() throws Exception {
+        super.testMapReplaceAllJDK7();
+    }
+
+    @Test
+    public void testMapReplaceAll() throws Exception {
+        super.testMapReplaceAll();
+    }
+
+    @Test
+    public void testMapReplaceAllLambda() throws Exception {
+        super.testMapReplaceAllLambda();
+    }
+
+    @Test
+    public void testMapComputeIfAbsent() throws Exception {
+        super.testMapComputeIfAbsent();
+    }
+
+}
